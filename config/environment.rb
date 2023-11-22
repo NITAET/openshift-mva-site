9db1183614b4afdc9e0b40d3d3cc044cc2357b8b
NITAET <nitaet@gmail.com>
2023-11-22 13:05:14 +0200

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
-# Load the Rails application.
-require_relative "application"
+# Pin npm packages by running ./bin/importmap
 
-# Initialize the Rails application.
-Rails.application.initialize!
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
